1dc0d3135b87e2fd3a716344ba791f7d204ed74f
cardosofiles <cardosofiles@outlook.com>
2024-10-28 17:12:08 -0300

    fetch products on home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+import { api } from "@/data/api";
+import { Product } from "@/data/types/products";
+
+async function getFeatureProducts(): Promise<Product[]> {
+  const response = await api("/products/featured");
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function Home() {
+  const [highLightedProduct, ...otherProducts] = await getFeatureProducts();
+
   return (
     <div className="grid max-h-[800px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={"/"}
+        href={`/products/${highLightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
       >
         <Image
-          src="/moletom-never-stop-learning 1.png"
+          src={highLightedProduct.image}
           alt=""
           width={920}
           height={920}
@@ -18,54 +30,48 @@ export default function Home() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] border-2 border-zinc-500 rounded-full bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moleton Never Stop-Learning</span>
+          <span className="text-sm truncate">{highLightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129,90
+            {highLightedProduct.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              maximumFractionDigits: 0,
+              minimumFractionDigits: 0,
+            })}
           </span>
         </div>
       </Link>
 
-      <Link
-        href={"/"}
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
-      >
-        <Image
-          src="/camiseta-dowhile-2022.png"
-          alt=""
-          width={920}
-          height={920}
-          quality={100}
-          className="group-hover:scale-105 transition transform duration-500"
-        />
+      {otherProducts.map((otherProduct) => {
+        return (
+          <Link
+            key={otherProduct.id}
+            href={`/product/${otherProduct.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
+          >
+            <Image
+              src={otherProduct.image}
+              alt=""
+              width={920}
+              height={920}
+              quality={100}
+              className="group-hover:scale-105 transition transform duration-500"
+            />
 
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] border-2 border-zinc-500 rounded-full bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Camiseta Dowhile 2022</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129,90
-          </span>
-        </div>
-      </Link>
-
-      <Link
-        href={"/"}
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
-      >
-        <Image
-          src="/moletom-ai-side.png"
-          alt=""
-          width={920}
-          height={920}
-          quality={100}
-          className="group-hover:scale-105 transition transform duration-500"
-        />
-
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] border-2 border-zinc-500 rounded-full bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moleton AI Side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129,90
-          </span>
-        </div>
-      </Link>
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] border-2 border-zinc-500 rounded-full bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{otherProduct.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {otherProduct.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                  maximumFractionDigits: 0,
+                  minimumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
